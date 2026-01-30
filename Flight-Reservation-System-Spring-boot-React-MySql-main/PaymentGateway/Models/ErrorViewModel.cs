7039bcbc3aa4e0fbc7b3758b54659ee6c75c0d39
YashodaPatil83 <yashodapatil83@gmail.com>
2026-01-30 13:05:38 +0530

    fully working project added for deploy

--- Flight-Reservation-System-Spring-boot-React-MySql-main/PaymentGateway/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PaymentGetway.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
